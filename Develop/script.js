1b3b5e54d16bbe53e7f8320bc03b438b83824975
Amanda Hamilton <77996281+polysnacktyl@users.noreply.github.com>
2021-03-15 10:40:29 -0400

    got input to save to local storage. still need to pull up display and color text field relative to time of day. also readme.

--- Develop/script.js
+++ Develop/script.js
@@ -5,31 +5,40 @@ console.log(Today)
 
 
 
-var taskEntry = document.getElementById("taskField");
-function saveTask() {
-    var newTask = taskEntry.textContent.trim();
-    localStorage.setItem("newTask", JSON.stringify(newTask));
-}
 
-function displayTask () {
+
+function displayTasks() {
+    // Get list of keys (maybe from the DOM? idk how did we get the key when we saved them?)
+    $(".hour").each(function() {
+        // Okay I have my $(this). How do get from that to key? "9:00am"
+        console.log("ldfjklaskfjs"); // Set a break point here and poke at things
+        // Once i get from that to key, read that key out of local storage
+        // Then, find the sibling of $(this) into which I need to put the task I just read from local Storage
+
+
+    });
+
+    // For each key: 
+    //      read the value from local storage
+    //      then set the right DOM element's innerText to that value
     var currentTasks = JSON.parse(localStorage.getItem("saveTask"));
     if (currentTasks !== null) {
         document.getElementById("taskField").innerHTML = currentTasks;
     }
 } 
 
-function hearButton() {
-var taskButton = document.getElementById("taskButton")
-taskButton.addEventListener("click", function(event) {
-    event.preventDefault();
-    saveTask();
-    displayTask();
-});
-}
 
-function init() {
-    saveTask();
-    displayTask();
-}
+$( ".saveBtn" ).click(function() {
+    // once i'm in here: figure out if the button is actually clicked 
+    //if not, return 
+    //if yes click, figure out what inner text or whatever you can glean from that 
+    //
+    // alert( "Handler for .click() called." );
+    var time = $(this).siblings(".hour")[0].innerText.trim();
+    var task = $(this).siblings(".time-block")[0].innerText.trim();
 
-init();
+    localStorage.setItem(time, JSON.stringify(task));
+
+  });
+
+  displayTasks();

cf4a3548adbee9f9703167b20f33c868d518b01e
Amanda Hamilton <the.a.hamilton@gmail.com>
2021-03-15 20:35:17 -0400

    making slight progress on retrieving local storage items--key registering as undefined causing text var to return null--but at least it's doing that the proper number of times, so we'll call that a very tiny success

--- Develop/script.js
+++ Develop/script.js
@@ -3,7 +3,9 @@ var Today = moment().format('MMMM Do YYYY');
 $("#itsToday").text(Today);
 console.log(Today)
 
-var nowTask = (localStorage.getItem("10:00am"))
+
+
+
 
 
 //read localStorage Item (time, task) 
@@ -15,16 +17,30 @@ var nowTask = (localStorage.getItem("10:00am"))
 //---------------------------------------this works don't futz with it//
 $(".saveBtn").click(function () {
 
-var time = $(this).siblings(".hour")[0].innerText.trim();
-var task = $(this).siblings(".time-block")[0].innerText.trim();
+    var time = $(this).siblings(".hour")[0].innerText.trim();
+    var task = $(this).siblings(".time-block")[0].innerText.trim();
 
-localStorage.setItem(time, JSON.stringify(task));
+    localStorage.setItem(time, JSON.stringify(task));
 
-}); 
+});
 //----------------------------------------but seriously leave it alone//
 
 
 
 //yay i figured out how to at least get it to show up in console log
 //call the specific key that shows up in the local storage list of key/val pairs 
-console.log(localStorage.getItem("10:00am", ))
+// console.log(localStorage.getItem("9:00am"))
+// console.log(localStorage.getItem("10:00am"))
+// console.log(localStorage.getItem("11:00am"))
+
+$(".hour").each(function () {
+    var key = $(this).innerText;
+    var text = localStorage.getItem(key);
+    $(this).siblings(".time-block")[0].innerText = text;
+
+    console.log(key); //undefined
+    console.log(text);//returning null
+
+});
+
+
